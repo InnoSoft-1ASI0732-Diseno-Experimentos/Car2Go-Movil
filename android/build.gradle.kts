2a3d1d441fe5420a19ed19656f6088d5c5d68f91
LynnMeza <lynn770450@gmail.com>
2025-06-18 19:11:41 -0500

    feat:generate apk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -19,3 +31,4 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,72 @@
-import 'package:car_2_go/presentation/screens/auth/login_buyer_screen.dart';
 import 'package:flutter/material.dart';
-import 'core/services/auth_service.dart';
-import 'presentation/screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'presentation/screens/auth/first_screen.dart';
+import 'presentation/screens/auth/login_buyer_screen.dart';
+import 'presentation/screens/auth/login_screen.dart';
+import 'core/services/auth_service.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('(BG) Notificación: ${message.notification?.title}');
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _mensaje = "Esperando notificación...";
+
+  @override
+  void initState() {
+    super.initState();
+    _initFirebaseMessaging();
+  }
+
+  void _initFirebaseMessaging() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    await messaging.subscribeToTopic("alertas-dev");
+    print("✅ Suscrito a alertas-dev");
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('📲 Notificación en primer plano');
+      setState(() {
+        _mensaje = message.notification?.body ?? 'Sin contenido';
+      });
+
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text(message.notification?.title ?? 'Alerta'),
+          content: Text(message.notification?.body ?? 'Sin contenido'),
+          actions: [
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +77,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const RoleSelectionScreen(), // ← aquí arranca tu app
+      home: const RoleSelectionScreen(),
     );
   }
 }
